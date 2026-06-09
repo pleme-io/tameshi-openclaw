4f05233823f0b1e8b7bd8a5ac8580817f40930fa
drzln <luis.d@akeyless.io>
2026-06-08 18:11:49 -0700

    flake: follow substrate/nixpkgs (fleet nixpkgs alignment for cache reuse)

--- flake.nix
+++ flake.nix
@@ -2,10 +2,9 @@
   description = "Tameshi-OpenClaw - AI agent integrity attestation integration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.follows = "substrate/nixpkgs";
     substrate = {
       url = "github:pleme-io/substrate";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
     crate2nix = {
       url = "github:nix-community/crate2nix";
